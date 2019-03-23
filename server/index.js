f4580dee6d46d97ac6f8d4cf12a2fac58f1dbf7d
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2019-03-23 13:12:39 +0000

    remove mongoose

--- server/index.js
+++ server/index.js
@@ -5,21 +5,17 @@ const PORT = process.env.PORT || 3000;
 const dev = process.env.NODE_DEV !== "production"; //true false
 const nextApp = next({ dev });
 const handle = nextApp.getRequestHandler(); //part of next config
-// const mongoose = require("mongoose");
-
-// const db = mongoose.connect("mongodb://localhost:27017/Photos");
 
 nextApp.prepare().then(() => {
   // express code here
-  const app = express()
+  const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-  // app.use('/api/photos', require('./routes/index')) 
-  app.get('*', (req,res) => {
-      return handle(req,res) // for all the react stuff
-  })
+  app.get("*", (req, res) => {
+    return handle(req, res); // for all the react stuff
+  });
   app.listen(PORT, err => {
-      if (err) throw err;
-      console.log(`ready at http://localhost:${PORT}`)
-  })
-})
+    if (err) throw err;
+    console.log(`ready at http://localhost:${PORT}`);
+  });
+});
